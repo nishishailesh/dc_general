954f544dd81690f75ab660ed4fe0d9b9d4903c34
root <root@localhost>
2020-03-16 22:12:25 +0530

    ok

--- update_dc_general_to_git.sh
+++ update_dc_general_to_git.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #only blank
-mysqldump  -d -uroot dc_general -p > dc_general_blank.sql 
-for tname in examination profile report 
+read ppp
+mysqldump  -d -uroot dc_general -p$ppp > dc_general_blank.sql
+for tname in examination profile report
 do
-	mysqldump  -uroot dc_general $tname -p$password > "dc_general_$tname.sql"
+	mysqldump  -uroot dc_general $tname -p$ppp > "dc_general_$tname.sql"
 done
 git add *
 git commit
